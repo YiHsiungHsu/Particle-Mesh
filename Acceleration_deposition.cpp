213c6336b1396d72c8d92b84fc52cb928a86c9cc
YiHsiungHsu <arthurhsu3388@gmail.com>
2020-06-15 21:22:08 +0800

    Add acceleration deposition back to particles. Modified acceleration deposition.

--- Acceleration_deposition.cpp
+++ Acceleration_deposition.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-void acceleration_deposition( int N, float ***a_grid, double *x, double *y, double *z, double gs, int GN, int mode, double *a)
+void acceleration_deposition( int N, float ***a_grid, float ***M_grid, double *M, double *x, double *y, double *z, double gs, int GN, int mode, double *a)
 {
 /*
  mode 1: NGP
@@ -17,117 +17,140 @@ void acceleration_deposition( int N, float ***a_grid, double *x, double *y, doub
     for(int n = 0; n<N; n++){
         a[n] = 0.0;
     }
+    double m[N][N][N][N]; //allocated mass for every single particle with m[particle][gridx][gridy][gridz]
     double dx, dy, dz;
     double wx, wy, wz; //weighted function
     
     if(mode == 1)
-    {
-        for(int n = 0; n<N; n++)
         {
-            for(int i = 0; i<GN; i++)
+            for(int n = 0; n<N; n++)
             {
-                dx = fabs(x[n]-i*gs);
-                
-                if(dx<0.5*gs) wx = 1.0;
-                else if(dx==0.5*gs) wx = 0.5;
-                else wx = 0.0;
-                
-                for(int j = 0; j<GN; j++)
+                for(int i = 0; i<GN; i++)
                 {
-                    dy = fabs(y[n]-j*gs);
+                    dx = fabs(x[n]-i*gs);
                     
-                    if(dy<0.5*gs) wy = 1.0;
-                    else if(dy==0.5*gs) wy = 0.5;
-                    else wy = 0.0;
+                    if(dx<0.5*gs) wx = 1.0;
+                    else if(dx==0.5*gs) wx = 0.5;
+                    else wx = 0.0;
                     
-                    for(int k = 0; k<GN; k++)
+                    for(int j = 0; j<GN; j++)
                     {
-                        dz = fabs(z[n]-k*gs);
+                        dy = fabs(y[n]-j*gs);
                         
-                        if(dz<0.5*gs) wz = 1.0;
-                        else if(dz==0.5*gs) wz = 0.5;
-                        else wz = 0.0;
+                        if(dy<0.5*gs) wy = 1.0;
+                        else if(dy==0.5*gs) wy = 0.5;
+                        else wy = 0.0;
                         
-                        a[n] += wx*wy*wz*a_grid[i][j][k];
+                        for(int k = 0; k<GN; k++)
+                        {
+                            dz = fabs(z[n]-k*gs);
+                            
+                            if(dz<0.5*gs) wz = 1.0;
+                            else if(dz==0.5*gs) wz = 0.5;
+                            else wz = 0.0;
+                            
+                            m[n][i][j][k] = wx*wy*wz*M[n];
+                            if(m[n][i][j][k] != 0.0 && M_grid[i][j][k] != 0.0)
+                            {
+                                a[n] += m[n][i][j][k]/M_grid[i][j][k]*a_grid[i][j][k];
+                            }
+                            
+                        }
                     }
                 }
             }
         }
-    }
-    if(mode == 2)
-    {
-        for(int n = 0; n<N; n++)
+        if(mode == 2)
         {
-            for(int i = 0; i<GN; i++)
+            for(int n = 0; n<N; n++)
             {
-                dx = fabs(x[n]-i*gs);
-                
-                if(dx<gs) wx = (1.0-dx/gs);
-                else wx = 0.0;
-                
-                for(int j = 0; j<GN; j++)
+                for(int i = 0; i<GN; i++)
                 {
-                    dy = fabs(y[n]-j*gs);
-                    for(int k = 0; k<GN; k++)
+                    dx = fabs(x[n]-i*gs);
+                    
+                    if(dx<gs) wx = (1.0-dx/gs);
+                    else wx = 0.0;
+                    
+                    for(int j = 0; j<GN; j++)
                     {
-                        dz = fabs(z[n]-k*gs);
-                        if(dx<=gs && dy<=gs && dz<=gs)
+                        dy = fabs(y[n]-j*gs);
+                        
+                        if(dy<gs) wy = (1.0-dy/gs);
+                        else wy = 0.0;
+                        
+                        for(int k = 0; k<GN; k++)
                         {
-                            a[n] += (1.0-dx/gs)*(1.0-dy/gs)*(1.0-dz/gs)*a_grid[i][j][k];
+                            dz = fabs(z[n]-k*gs);
+                            
+                            if(dz<gs) wz = (1.0-dz/gs);
+                            else wz = 0.0;
+                            
+                            m[n][i][j][k] = wx*wy*wz*M[n];
+                            if(m[n][i][j][k] != 0.0 && M_grid[i][j][k] != 0.0)
+                            {
+                                a[n] += m[n][i][j][k]/M_grid[i][j][k]*a_grid[i][j][k];
+                            }
+                            
+                        }
+                    }
+                }
+            }
+        }
+        if(mode == 3)
+        {
+            for(int n = 0; n<N; n++)
+            {
+                for(int i = 0; i<GN; i++)
+                {
+                    dx = fabs(x[n]-i*gs);
+                    
+                    if(dx<0.5*gs)
+                    {
+                        wx = 0.75-dx*dx/gs/gs;
+                    }
+                    else if(dx>=0.5*gs && dx <= 1.5*gs)
+                    {
+                        wx = 0.5*(1.5-dx/gs)*(1.5-dx/gs);
+                    }
+                    else wx = 0.0;
+                    
+                    for(int j = 0; j<GN; j++)
+                    {
+                        dy = fabs(y[n]-j*gs);
+                        
+                        if(dy<0.5*gs)
+                        {
+                            wy = 0.75-dy*dy/gs/gs;
+                        }
+                        else if(dy>=0.5*gs && dy <= 1.5*gs)
+                        {
+                            wy = 0.5*(1.5-dy/gs)*(1.5-dy/gs);
+                        }
+                        else wy = 0.0;
+                        
+                        for(int k = 0; k<GN; k++)
+                        {
+                            dz = fabs(z[n]-k*gs);
+                            
+                            if(dz<0.5*gs)
+                            {
+                                wz = 0.75-dz*dz/gs/gs;
+                            }
+                            else if(dz>=0.5*gs && dz <= 1.5*gs)
+                            {
+                                wz = 0.5*(1.5-dz/gs)*(1.5-dz/gs);
+                            }
+                            else wz = 0.0;
+                            
+                            m[n][i][j][k] = wx*wy*wz*M[n];
+                            if(m[n][i][j][k] != 0.0 && M_grid[i][j][k] != 0.0)
+                            {
+                                a[n] += m[n][i][j][k]/M_grid[i][j][k]*a_grid[i][j][k];
+                            }
                         }
                     }
                 }
             }
         }
     }
-    if(mode == 3)
-    {
-        for(int n = 0; n<N; n++)
-        {
-            for(int i = 0; i<GN; i++)
-            {
-                dx = fabs(x[n]-i*gs);
-                if(dx<=0.5*gs)
-                {
-                    wx = 0.75-dx*dx/gs/gs;
-                }
-                else if(dx>=0.5*gs && dx <= 1.5*gs)
-                {
-                    wx = 0.5*(1.5-dx/gs)*(1.5-dx/gs);
-                }
-                else wx = 0.0;
-                
-                for(int j = 0; j<GN; j++)
-                {
-                    dy = fabs(y[n]-j*gs);
-                    if(dy<=0.5*gs)
-                    {
-                        wy = 0.75-dy*dy/gs/gs;
-                    }
-                    else if(dy>=0.5*gs && dy <= 1.5*gs)
-                    {
-                        wy = 0.5*(1.5-dy/gs)*(1.5-dy/gs);
-                    }
-                    else wy = 0.0;
-                    
-                    for(int k = 0; k<=GN; k++)
-                    {
-                        dz = fabs(z[n]-k*gs);
-                        if(dz<=0.5*gs)
-                        {
-                            wz = 0.75-dz*dz/gs/gs;
-                        }
-                        else if(dz>=0.5*gs && dz <= 1.5*gs)
-                        {
-                            wz = 0.5*(1.5-dz/gs)*(1.5-dz/gs);
-                        }
-                        else wz = 0.0;
-                        
-                        a[n] += wx*wy*wz*a_grid[i][j][k];
-                        
-                    }
-                }
-            }
-        }
-    }
-}
+
--- Particle_Mesh.cpp
+++ Particle_Mesh.cpp
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+//include any package you need and omp or mpi
 
 float ***buildGrid(const int numRows, const int numCols, const int numLevels); //creat grid points
 void mass_deposition( const int N, double *M, double *x, double *y, double *z, const double gs, const int GN, const int mode, float ****M_grid);
+void acceleration_deposition( int N, float ***a_grid, float ***M_grid, double *M, double *x, double *y, double *z, double gs, int GN, int mode, double *a);
 
 int main(void){
     // constants
@@ -48,12 +50,19 @@ int main(void){
     
         // acceleration deposition here
         // I expect my output to be ax[N], ay[N], az[N]
-        // There are still some problems here.
+        double a[N];
+        float ***a_grid = buildGrid(GN,GN,GN);
+        //assign a_grid here
+        void acceleration_deposition( int N, float ***a_grid, float ***M_grid, double *M, double *x, double *y, double *z, double gs, int GN, int mode, double *a);
         // end acceleration deopsotion
         
         // Hermite Integral, DKD, KDK
         // Read the output of acceleration deposition and see if there should be any change.
         // end HI, DKD, KDK
+        
+        // Dump data
+        
+        // end dump data
         
         t += ts;
     }
@@ -234,3 +243,155 @@ void mass_deposition(int N, double *M, double *x, double *y, double *z, double g
         }
     }
 }
+
+void acceleration_deposition( int N, float ***a_grid, float ***M_grid, double *M, double *x, double *y, double *z, double gs, int GN, int mode, double *a)
+{
+/*
+ mode 1: NGP
+ mode 2: CIC
+ mode 3: TSC
+ N is total number of particles.
+ gs is grid size.
+ GN is total grid number. Note that I assume we have a equilateral box.
+ a_grid is the input acceleration matrix.
+ a is output acceleration of one component for every particle. (a zero matrix pointer)
+*/
+    for(int n = 0; n<N; n++){
+        a[n] = 0.0;
+    }
+    double m[N][N][N][N]; //allocated mass for every single particle with m[particle][gridx][gridy][gridz]
+    double dx, dy, dz;
+    double wx, wy, wz; //weighted function
+    
+    if(mode == 1)
+        {
+            for(int n = 0; n<N; n++)
+            {
+                for(int i = 0; i<GN; i++)
+                {
+                    dx = fabs(x[n]-i*gs);
+                    
+                    if(dx<0.5*gs) wx = 1.0;
+                    else if(dx==0.5*gs) wx = 0.5;
+                    else wx = 0.0;
+                    
+                    for(int j = 0; j<GN; j++)
+                    {
+                        dy = fabs(y[n]-j*gs);
+                        
+                        if(dy<0.5*gs) wy = 1.0;
+                        else if(dy==0.5*gs) wy = 0.5;
+                        else wy = 0.0;
+                        
+                        for(int k = 0; k<GN; k++)
+                        {
+                            dz = fabs(z[n]-k*gs);
+                            
+                            if(dz<0.5*gs) wz = 1.0;
+                            else if(dz==0.5*gs) wz = 0.5;
+                            else wz = 0.0;
+                            
+                            m[n][i][j][k] = wx*wy*wz*M[n];
+                            if(m[n][i][j][k] != 0.0 && M_grid[i][j][k] != 0.0)
+                            {
+                                a[n] += m[n][i][j][k]/M_grid[i][j][k]*a_grid[i][j][k];
+                            }
+                            
+                        }
+                    }
+                }
+            }
+        }
+        if(mode == 2)
+        {
+            for(int n = 0; n<N; n++)
+            {
+                for(int i = 0; i<GN; i++)
+                {
+                    dx = fabs(x[n]-i*gs);
+                    
+                    if(dx<gs) wx = (1.0-dx/gs);
+                    else wx = 0.0;
+                    
+                    for(int j = 0; j<GN; j++)
+                    {
+                        dy = fabs(y[n]-j*gs);
+                        
+                        if(dy<gs) wy = (1.0-dy/gs);
+                        else wy = 0.0;
+                        
+                        for(int k = 0; k<GN; k++)
+                        {
+                            dz = fabs(z[n]-k*gs);
+                            
+                            if(dz<gs) wz = (1.0-dz/gs);
+                            else wz = 0.0;
+                            
+                            m[n][i][j][k] = wx*wy*wz*M[n];
+                            if(m[n][i][j][k] != 0.0 && M_grid[i][j][k] != 0.0)
+                            {
+                                a[n] += m[n][i][j][k]/M_grid[i][j][k]*a_grid[i][j][k];
+                            }
+                            
+                        }
+                    }
+                }
+            }
+        }
+        if(mode == 3)
+        {
+            for(int n = 0; n<N; n++)
+            {
+                for(int i = 0; i<GN; i++)
+                {
+                    dx = fabs(x[n]-i*gs);
+                    
+                    if(dx<0.5*gs)
+                    {
+                        wx = 0.75-dx*dx/gs/gs;
+                    }
+                    else if(dx>=0.5*gs && dx <= 1.5*gs)
+                    {
+                        wx = 0.5*(1.5-dx/gs)*(1.5-dx/gs);
+                    }
+                    else wx = 0.0;
+                    
+                    for(int j = 0; j<GN; j++)
+                    {
+                        dy = fabs(y[n]-j*gs);
+                        
+                        if(dy<0.5*gs)
+                        {
+                            wy = 0.75-dy*dy/gs/gs;
+                        }
+                        else if(dy>=0.5*gs && dy <= 1.5*gs)
+                        {
+                            wy = 0.5*(1.5-dy/gs)*(1.5-dy/gs);
+                        }
+                        else wy = 0.0;
+                        
+                        for(int k = 0; k<GN; k++)
+                        {
+                            dz = fabs(z[n]-k*gs);
+                            
+                            if(dz<0.5*gs)
+                            {
+                                wz = 0.75-dz*dz/gs/gs;
+                            }
+                            else if(dz>=0.5*gs && dz <= 1.5*gs)
+                            {
+                                wz = 0.5*(1.5-dz/gs)*(1.5-dz/gs);
+                            }
+                            else wz = 0.0;
+                            
+                            m[n][i][j][k] = wx*wy*wz*M[n];
+                            if(m[n][i][j][k] != 0.0 && M_grid[i][j][k] != 0.0)
+                            {
+                                a[n] += m[n][i][j][k]/M_grid[i][j][k]*a_grid[i][j][k];
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
