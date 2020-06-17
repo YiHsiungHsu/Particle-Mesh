9b5ca35c83a097c5faec675bf5cc2aa04f809788
YiGuangLee <66617238+YiGuangLee@users.noreply.github.com>
2020-06-17 16:49:32 +0800

    fit it with main code

--- hermiteKDK.cpp
+++ hermiteKDK.cpp
@@ -11,6 +11,306 @@ double position[N][3]; //positions of bodies in 3-dimension, [N][0] for x, [N][1
 double velocity[N][3]; //velocity of bodies in 3-dimension, [N][0] for x, [N][1] for y, [N][2] for z
 double mass[N];        //mass of bodies in 3-dimension
 
+void hermite( const int N, double *M, double *x, double *y, double *z, double *vx, double *vy, double *vz, double *ax, double *ay, double *az, double *jx, double *jy, double *jz, double gs, const int GN  );
+
+void hermite( const int N, double *M, double *x, double *y, double *z, double *vx, double *vy, double *vz, double *ax, double *ay, double *az, double *dax, double *day, double *daz, double gs, const int GN  )
+{
+float dt[N];         //timestep based on all particles' properties respectively.
+
+double rx;
+
+double ry;
+
+double rz;
+
+double rvx;
+
+double rvy;
+
+double rvz;
+
+double ahx[N];
+
+double ahy[N];
+
+double ahz[N];
+
+double jhx[N];
+
+double jhy[N];
+
+double jhz[N];
+
+double afx[N];//f stand for final, the predicted acceleration at time t+ts
+
+double afy[N];
+
+double afz[N];
+
+double jfx[N];
+
+double jfy[N];
+
+double jfz[N];
+
+double a2x[N]; //second order derivative of acceleration
+
+double a2y[N];
+
+double a2z[N];
+
+double a3x[N]; //third order derivative of acceleration
+
+double a3y[N];
+
+double a3z[N];
+
+double af2x[N];   //second derivative of acceleration at t + dt
+
+double af2y[N];
+
+double af2z[N];
+
+double aaf[N];   //first a of all below stands for absolute value
+
+double aaf2[N];
+
+double ajf[N];
+
+double aa3[N];
+
+double rex = 0;
+
+double rey = 0;
+
+double rez = 0;
+
+double hts = ts/2;
+
+   for (int i=0; i<N; i++){//parameters initialization
+
+      aaf[i] = 0;
+
+      aaf2[i] = 0;
+
+      ajf[i] = 0;
+
+      aa3[i] = 0;
+
+      ahx[i] = 0;
+
+      ahy[i] = 0;
+
+      ahz[i] = 0;
+
+      jhx[i] = 0;
+
+      jhy[i] = 0;
+
+      jhz[i] = 0;
+
+      afx[i] = 0; 
+
+      afy[i] = 0;
+
+      afz[i] = 0;
+
+      jfx[i] = 0;
+
+      jfy[i] = 0;
+
+      jfz[i] = 0;
+
+      a2x[i] = 0;
+
+      a2y[i] = 0;
+
+      a2z[i] = 0;
+
+      a3x[i] = 0;
+
+      a3y[i] = 0;
+
+      a3z[i] = 0;
+
+      af2x[i] = 0;
+
+      af2y[i] = 0;
+
+      af2z[i] = 0;
+                          }
+
+//first kick of 0.5ts
+   for (n=0; n<N; n++){
+      vx[n] += ( hts*ax[n] + pow(hts, 2)*jx[n]/2 );
+
+      vy[n] += ( hts*ay[n] + pow(hts, 2)*jy[n]/2 );
+
+      vz[n] += ( hts*az[n] + pow(hts, 2)*jz[n]/2 );
+		      }
+
+//now update a and jerk at t+hts
+   for (n=0; n<N; n++){
+   for (j=0; j<N; j++){
+   if  (n != j)       {
+      rx = fabs(x[n]-x[j]);
+
+      ry = fabs(y[n]-y[j]);
+
+      rz = fabs(z[n]-z[j]);
+
+      rex = pow(rx, 2)+pow(sp, 2);
+
+      rey = pow(ry, 2)+pow(sp, 2);
+
+      rez = pow(rz, 2)+pow(sp, 2);
+
+      ahx[n] += G*M[j]*rx/sqrt(pow(rex, 3));
+
+      ahy[n] += G*M[j]*ry/sqrt(pow(rey, 3));
+
+      ahz[n] += G*M[j]*rz/sqrt(pow(rez, 3));
+
+      rvx = fabs(vx[n] - vx[j]);
+
+      rvy = fabs(vy[n] - vy[j]);
+
+      rvz = fabs(vz[n] - vz[j]);
+
+      jhx[n] += G*M[j]*(rvx/sqrt(pow(rex, 3)) + 3*(rvx*rx)*rx/sqrt(pow(rex, 5)));
+
+      jhy[n] += G*M[j]*(rvy/sqrt(pow(rey, 3)) + 3*(rvy*ry)*ry/sqrt(pow(rey, 5)));
+
+      jhz[n] += G*M[j]*(rvz/sqrt(pow(rez, 3)) + 3*(rvz*rz)*rz/sqrt(pow(rez, 5)));
+		      }
+		      }
+		      }
+
+//drift and second kick
+   for (n=0; n<N; n++){
+      x[n] += ( ts*vx[n] + pow(ts, 2)*ahx[n]/2 + pow(ts, 3)*jhx[n]/6 );
+
+      y[n] += ( ts*vy[n] + pow(ts, 2)*ahy[n]/2 + pow(ts, 3)*jhy[n]/6 );
+
+      z[n] += ( ts*vz[n] + pow(ts, 2)*ahz[n]/2 + pow(ts, 3)*jhz[n]/6 );
+
+      vx[n] += ( hts*ahx[n] + pow(hts, 2)*jhx[n]/2 );
+
+      vy[n] += ( hts*ahy[n] + pow(hts, 2)*jhy[n]/2 );
+
+      vz[n] += ( hts*ahz[n] + pow(hts, 2)*jhz[n]/2 );
+		      }
+
+    }
+
+//predict a and jerk at time t + ts
+   for (int n=0; n<N; n++){
+   for (int j=0; j<N; j++){
+   if  (n != j)           {
+      rx = fabs(x[n]-x[j]);
+
+      ry = fabs(y[n]-y[j]);
+
+      rz = fabs(z[n]-z[j]);
+
+      rex = pow(rx, 2)+pow(sp, 2);
+
+      rey = pow(ry, 2)+pow(sp, 2);
+
+      rez = pow(rz, 2)+pow(sp, 2);
+
+      afx[n] += G*M[j]*rx/sqrt(pow(rex, 3));
+
+      afy[n] += G*M[j]*ry/sqrt(pow(rey, 3));
+
+      afz[n] += G*M[j]*rz/sqrt(pow(rez, 3));
+
+      rvx = fabs(vx[n] - vx[j]);
+
+      rvy = fabs(vy[n] - vy[j]);
+
+      rvz = fabs(vz[n] - vz[j]);
+
+      jfx[n] += G*M[j]*(rvx/sqrt(pow(rex, 3)) + 3*(rvx*rx)*rx/sqrt(pow(rex, 5)));
+
+      jfy[n] += G*M[j]*(rvy/sqrt(pow(rey, 3)) + 3*(rvy*ry)*ry/sqrt(pow(rey, 5)));
+
+      jfz[n] += G*M[j]*(rvz/sqrt(pow(rez, 3)) + 3*(rvz*rz)*rz/sqrt(pow(rez, 5)));
+       			   }
+			   }
+      			   }
+
+//high order correction
+   for(int n=0; n<N; n++){
+      a2x[n] = ( 6*(afx[n] - ax[n]) - ts*(4*jx[n] + 2*jfx[n]) )/(pow(ts, 2)+1e-7);
+
+      a2y[n] = ( 6*(afy[n] - ay[n]) - ts*(4*jy[n] + 2*jfy[n]) )/(pow(ts, 2)+1e-7);
+
+      a2z[n] = ( 6*(afz[n] - az[n]) - ts*(4*jz[n] + 2*jfz[n]) )/(pow(ts, 2)+1e-7);
+
+      a3x[n] = ( 12*(ax[n] - afx[n]) - 6*ts*(jx[n] + jfx[n]) )/(pow(ts, 3)+1e-7);
+
+      a3y[n] = ( 12*(ay[n] - afy[n]) - 6*ts*(jy[n] + jfy[n]) )/(pow(ts, 3)+1e-7);
+
+      a3z[n] = ( 12*(az[n] - afz[n]) - 6*ts*(jz[n] + jfz[n]) )/(pow(ts, 3)+1e-7);
+                          }
+
+//final acceleration, position and velocity
+  for (int n=0; n<N; n++){
+      ax[n] += ( ts*jx[n] + pow(ts, 2)*a2x[n]/2 + pow(ts, 3)*a3x[n]/6 );
+
+      ay[n] += ( ts*jy[n] + pow(ts, 2)*a2y[n]/2 + pow(ts, 3)*a3y[n]/6 );
+
+      az[n] += ( ts*jz[n] + pow(ts, 2)*a2z[n]/2 + pow(ts, 3)*a3z[n]/6 );
+
+      jx[n] = jfx[n];
+
+      jy[n] = jfy[n];
+
+      jz[n] = jfz[n];
+
+      x[n] += ( pow(ts, 4)*a2x[n]/24 + pow(ts, 5)*a3x[n]/120 );
+
+      y[n] += ( pow(ts, 4)*a2y[n]/24 + pow(ts, 5)*a3y[n]/120 );
+
+      z[n] += ( pow(ts, 4)*a2z[n]/24 + pow(ts, 5)*a3z[n]/120 );
+
+      vx[n] += ( pow(ts, 3)*a2x[n]/6 + pow(ts, 4)*a3x[n]/24 );
+
+      vy[n] += ( pow(ts, 3)*a2y[n]/6 + pow(ts, 4)*a3y[n]/24 );
+
+      vz[n] += ( pow(ts, 3)*a2z[n]/6 + pow(ts, 4)*a3z[n]/24 );
+
+      af2x[n] = a2x[n] + ts*a3x[n];
+
+      af2y[n] = a2y[n] + ts*a3y[n];
+
+      af2z[n] = a2z[n] + ts*a3z[n];
+                          }
+
+//new timestep to next step
+   for (int n=0; n<N; n++){
+      aaf[n]  = sqrt((pow(ax[n], 2) + pow(ay[n], 2) + pow(az[n], 2)));
+
+      ajf[n] = sqrt((pow(jx[n], 2) + pow(jy[n], 2) + pow(jz[n], 2)));
+
+      aa3[n]  = sqrt((pow(a3x[n], 2) + pow(a3y[n], 2) + pow(a3z[n], 2)));
+
+      aaf2[n] = sqrt((pow(af2x[n], 2) + pow(af2y[n], 2) + pow(af2z[n], 2)));
+
+      dt[n]   = sqrt(0.01*((aaf[n]*aaf2[n] + pow(ajf[n], 2))/(ajf[n]*aa3[n] + pow(aaf2[n], 2) + 1e-7)));
+			  }
+
+   ts = dt[0];
+
+   for (int i=0; i<N; i++){
+   if  ( dt[i] < ts )     {
+       ts = dt[i];
+       if ( ts < 1e-6 ) ts = 1e-7;
+			  }
+                          }
+}
+/*
 void hermite(double position, double velocity, double mass)
 {
 //initial acceleration calculate
@@ -142,4 +442,4 @@ while ( t<timescale ){
    t += tmin;
 //if t < given time scale, return to beginning of while loop, start another iteration.
 	}
-}
+}*/
