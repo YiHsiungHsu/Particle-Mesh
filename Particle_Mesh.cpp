d92c84a5cb89bdef19473333b0ee7df8b2905dd9
r08244004 <64837806+r08244004@users.noreply.github.com>
2020-06-18 17:45:28 +0800

    change Potential.cpp variable name

--- Particle_Mesh.cpp
+++ Particle_Mesh.cpp
@@ -511,33 +511,33 @@ void Potential( double *rho, double *phi )
   {
     //fft rho to rhok
     fftw_complex *rhok;
-    rhok = (fftw_complex*) fftw_malloc( N*N*(N/2+1) * sizeof(fftw_complex) );
+    rhok = (fftw_complex*) fftw_malloc( GN*GN*(GN/2+1) * sizeof(fftw_complex) );
     fftw_plan fft;
-    fft = fftw_plan_dft_r2c_3d( N, N, N, rho, rhok, FFTW_ESTIMATE);
+    fft = fftw_plan_dft_r2c_3d( GN, GN, GN, rho, rhok, FFTW_ESTIMATE);
     fftw_execute(fft);
     //calculat potential phi =-4*M_PI*G/(kx**2+ky**2+kz**2)
     //need normailze with 1/N*N*N
     double _n;
-    _n = 1 / (N*N*N);                                //normalize factor
+    _n = 1 / (GN*GN*GN);                                //normalize factor
     fftw_complex *phik;
-    phik = (fftw_complex*) fftw_malloc( N*N*(N/2+1) * sizeof(fftw_complex) );
-    for(int i = 0; i < N; i++)
-    for(int j = 0; j < N; j++)
-    for(int k = 0; k < (N/2+1); k++)
+    phik = (fftw_complex*) fftw_malloc( GN*GN*(GN/2+1) * sizeof(fftw_complex) );
+    for(int i = 0; i < GN; i++)
+    for(int j = 0; j < GN; j++)
+    for(int k = 0; k < (GN/2+1); k++)
     {
         double _k;
         double kx,ky,kz;
-        if (i > N/2)
+        if (i > GN/2)
         {   
-           kx = N-i;
+           kx = GN-i;
         }
         else
         {
            kx = i;
         }
-        if (j > N/2)
+        if (j > GN/2)
         {
-           ky = N-j;
+           ky = GN-j;
         }
         else
         {
@@ -545,15 +545,15 @@ void Potential( double *rho, double *phi )
         }
         kz = k;
         _k = -1/((kx*kx)+(ky*ky)+(kz*kz));
-        phik[k+(N/2+1)*(j+N*i)][0] = 4*M_PI*G*_k*_n*rhok[k+(N/2+1)*(j+N*i)][0];  //real part
-        phik[k+(N/2+1)*(j+N*i)][1] = 4*M_PI*G*_k*_n*rhok[k+(N/2+1)*(j+N*i)][1];  //imagine part
+        phik[k+(GN/2+1)*(j+GN*i)][0] = 4*M_PI*G*_k*_n*rhok[k+(GN/2+1)*(j+GN*i)][0];  //real part
+        phik[k+(GN/2+1)*(j+GN*i)][1] = 4*M_PI*G*_k*_n*rhok[k+(GN/2+1)*(j+GN*i)][1];  //imagine part
     }
     //DC = 0
     phik[0][0] = 0; 
     phik[0][1] = 0;
     //fft phik to phi
     fftw_plan ifft;
-    ifft = fftw_plan_dft_c2r_3d( N, N, N, phik, phi, FFTW_ESTIMATE);  
+    ifft = fftw_plan_dft_c2r_3d( GN, GN, GN, phik, phi, FFTW_ESTIMATE);  
     fftw_execute(ifft);
     fftw_destroy_plan(fft)
     fftw_destroy_plan(ifft)
@@ -564,99 +564,99 @@ void Potential( double *rho, double *phi )
   {
      //zero padding M
      double *zM;         //zero padding M     
-     zM = (double*) fftw_malloc( (2*N)*(2*N)*(2*N) * sizeof(double) );
-     for (int i = 0; i < 2*N; i++)
-     for (int j = 0; j < 2*N; j++)
-     for (int k = 0; k < 2*N; k++)
+     zM = (double*) fftw_malloc( (2*GN)*(2*GN)*(2*GN) * sizeof(double) );
+     for (int i = 0; i < 2*GN; i++)
+     for (int j = 0; j < 2*GN; j++)
+     for (int k = 0; k < 2*GN; k++)
      {
-         zM[k+(2*N)*(j+2*N*i)] = 0.0;
+         zM[k+(2*N)*(j+2*GN*i)] = 0.0;
      }
-     for (int i = 0; i < N; i++)
-     for (int j = 0; j < N; j++)
-     for (int k = 0; k < N; k++)
+     for (int i = 0; i < GN; i++)
+     for (int j = 0; j < GN; j++)
+     for (int k = 0; k < GN; k++)
      {
-         zM[k+(2*N)*(j+2*N*i)] = rho[k+N*(j+N*i)]*dx*dx*dx;
+         zM[k+(2*GN)*(j+2*GN*i)] = rho[k+GN*(j+GN*i)]*gs*gs*gs;
      }
      double *dgf;        //discrete Green's function
-     dgf = (double*) fftw_malloc( (2*N)*(2*N)*(2*N) * sizeof(double) );   // dgf = -1*/R
-     for (int i = 0; i < 2*N; i++)
-     for (int j = 0; j < 2*N; j++)
-     for (int k = 0; k < 2*N; k++)
+     dgf = (double*) fftw_malloc( (2*GN)*(2*GN)*(2*GN) * sizeof(double) );   // dgf = -1*/R
+     for (int i = 0; i < 2*GN; i++)
+     for (int j = 0; j < 2*GN; j++)
+     for (int k = 0; k < 2*GN; k++)
      {
          double nx,ny,nz;
-         if (i > N )
+         if (i > GN )
             {
-               nx = 2*N - i;
+               nx = 2*GN - i;
             }
          else
             {
                nx = i;
             }
-         if (i > N )
+         if (i > GN )
             {
-               ny = 2*N - j;
+               ny = 2*GN - j;
             }
          else
             {
                ny = j;
             }
 
-         if (i > N )
+         if (i > GN )
             {
-               nz = 2*N - k;
+               nz = 2*GN - k;
             }
          else
             {
                nz = k;
             }
          double _R;
-         _R = -1 / dx*sqrt(nx*nx + ny*ny + nz*nz);
+         _R = -1 / gs*sqrt(nx*nx + ny*ny + nz*nz);
          if (i == 0 && j == 0 && k == 0)
             {
-               dgf[k+(2*N)*(j+2*N*i)] = 0.0;
+               dgf[k+(2*GN)*(j+2*GN*i)] = 0.0;
             }
          else
             {
-                dgf[k+(2*N)*(j+2*N*i)] = _R;
+                dgf[k+(2*GN)*(j+2*GN*i)] = _R;
             }
       }   
       //  FFT
       fftw_complex zMk,dftk;
-      zMK = (fftw_complex*) fftw_malloc( (2*N)*(2*N)*(N+1) * sizeof(fftw_complex) );
-      dftk = (fftw_complex*) fftw_malloc( (2*N)*(2*N)*(N+1) * sizeof(fftw_complex) );
+      zMK = (fftw_complex*) fftw_malloc( (2*GN)*(2*GN)*(GN+1) * sizeof(fftw_complex) );
+      dftk = (fftw_complex*) fftw_malloc( (2*GN)*(2*GN)*(GN+1) * sizeof(fftw_complex) );
       fftw_plan fftM, fftR;
-      fftM = fftw_plan_dft_r2c_3d( 2*N, 2*N, 2*N, zM, zMk, FFTW_ESTIMATE );
-      fftR = fftw_plan_dft_r2c_3d( 2*N, 2*N, 2*N, dft, dftk, FFTW_ESTIMATE );
+      fftM = fftw_plan_dft_r2c_3d( 2*GN, 2*GN, 2*GN, zM, zMk, FFTW_ESTIMATE );
+      fftR = fftw_plan_dft_r2c_3d( 2*GN, 2*GN, 2*GN, dft, dftk, FFTW_ESTIMATE );
       fftw_execute(fftM);
       fftw_execute(fftR);
       // convolution to get phi  ( a+bi * c+di )
       fftw_complex conk,phik;
-      conk = (fftw_complex*) fftw_malloc( (2*N)*(2*N)*(N+1) * sizeof(fftw_complex) );
-      phik = (fftw_complex*) fftw_malloc( (2*N)*(2*N)*(N+1) * sizeof(fftw_complex) );
-      for (int i = 0; i < 2*N; i++)
-      for (int j = 0; j < 2*N; j++)
-      for (int k = 0; k < N+1; k++)
+      conk = (fftw_complex*) fftw_malloc( (2*GN)*(2*GN)*(GN+1) * sizeof(fftw_complex) );
+      phik = (fftw_complex*) fftw_malloc( (2*GN)*(2*GN)*(GN+1) * sizeof(fftw_complex) );
+      for (int i = 0; i < 2*GN; i++)
+      for (int j = 0; j < 2*GN; j++)
+      for (int k = 0; k < GN+1; k++)
       {
-          conk[k+(N+1)*(j+2*N*i)][0] = (zMk[k+(N+1)*(j+2*N*i)][0] * dftk[k+(N+1)*(j+2*N*i)][0]) - (zMk[k+(N+1)*(j+2*N*i)][1] * dftk[k+(N+1)*(j+2*N*i)][1]);  // real part 
-          conk[k+(N+1)*(j+2*N*i)][1] = (zMk[k+(N+1)*(j+2*N*i)][0] * dftk[k+(N+1)*(j+2*N*i)][1]) + (zMk[k+(N+1)*(j+2*N*i)][1] * dftk[k+(N+1)*(j+2*N*i)][0]);  // imagine part
+          conk[k+(GN+1)*(j+2*GN*i)][0] = (zMk[k+(GN+1)*(j+2*GN*i)][0] * dftk[k+(GN+1)*(j+2*GN*i)][0]) - (zMk[k+(GN+1)*(j+2*GN*i)][1] * dftk[k+(GN+1)*(j+2*GN*i)][1]);  // real part 
+          conk[k+(GN+1)*(j+2*GN*i)][1] = (zMk[k+(GN+1)*(j+2*GN*i)][0] * dftk[k+(GN+1)*(j+2*GN*i)][1]) + (zMk[k+(GN+1)*(j+2*GN*i)][1] * dftk[k+(GN+1)*(j+2*GN*i)][0]);  // imagine part
       }
       double _n;
       _n = 1/(2*N*2*N*2*N );      //normailize factor
-      for (int i = 0; i < 2*N; i++)
-      for (int j = 0; j < 2*N; j++)
-      for (int k = 0; k < N+1; k++)
+      for (int i = 0; i < 2*GN; i++)
+      for (int j = 0; j < 2*GN; j++)
+      for (int k = 0; k < GN+1; k++)
       {
-          phik[k+(N+1)*(j+2*N*i)][0] = G*_n*conk[k+(N+1)*(j+2*N*i)][0];  //real part
-          phik[k+(N+1)*(j+2*N*i)][1] = G*_n*conk[k+(N+1)*(j+2*N*i)][1];  //imagine part
+          phik[k+(GN+1)*(j+2*GN*i)][0] = G*_n*conk[k+(GN+1)*(j+2*GN*i)][0];  //real part
+          phik[k+(GN+1)*(j+2*GN*i)][1] = G*_n*conk[k+(GN+1)*(j+2*GN*i)][1];  //imagine part
       }
       double *_phi; 
-      _phi = (double*) fftw_malloc( (2*N)(2*N)(2*N) * sizeof(double) );
+      _phi = (double*) fftw_malloc( (2*GN)(2*GN)(2*GN) * sizeof(double) );
       fftw_plan ifft;
-      ifft = fftw_plan_dft_c2r_3d( 2*N, 2*N, 2*N, phik, _phi, FFTW_ESTIMATE );
+      ifft = fftw_plan_dft_c2r_3d( 2*GN, 2*GN, 2*GN, phik, _phi, FFTW_ESTIMATE );
       fftw_execute(ifft);
-      for (int i = 0; i < N; i++)
-      for (int j = 0; j < N; j++)
-      for (int k = 0; k < N; k++)
+      for (int i = 0; i < GN; i++)
+      for (int j = 0; j < GN; j++)
+      for (int k = 0; k < GN; k++)
       {
          phi[k+N*(j+N*i)] = _phi[k+(2*N)*(j+2*N*i)];
       }
