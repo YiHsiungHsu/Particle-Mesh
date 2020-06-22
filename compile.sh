73384623c8b4f875210cb654e453f74034c2ea29
YiHsiungHsu <arthurhsu3388@gmail.com>
2020-06-22 17:41:26 +0800

    Add compile.

--- compile.sh
+++ compile.sh
@@ -1,21 +1,20 @@
 #usage: sh compile.sh filename.cpp
 
-# file name
+# filename
 IN=${1}
 
 # your compiler
-CC=g++
+CC=mpic++
 
-# openmp flag of your compiler 
+# openmp flag of your compiler
+OPENFLAG=-fopenmp
 
-#OPENFLAG=-openmp
-#OPENFLAG=-qopenmp
+#fftw path
 FFTW_PATH=/work1/jared/fftw
 
 # set the output name
 OUT=$(basename ${IN} .cpp).out
 
-${CC} -o ${OUT} ${IN} -L${FFTW_PATH}/lib -I${FFTW_PATH}/include  -lfftw3
-
+${CC} ${OPENFLAG} ${IN} -o ${OUT} -L${FFTW_PATH}/lib -I${FFTW_PATH}/include  -lfftw3
 
 #g++ -o play play.cpp -L/work1/jared/fftw/lib -I/work1/jared/fftw/include -lfftw3
