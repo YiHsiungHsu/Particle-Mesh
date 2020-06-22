125dbbc0f9716a7b79ac2a8bdc10c6950ff63c5b
r08244004 <64837806+r08244004@users.noreply.github.com>
2020-06-22 16:36:27 +0800

    Add files via upload

--- plot.py
+++ plot.py
@@ -10,9 +10,8 @@ import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d.axes3d as p3
 import matplotlib.animation as animation
 dt = 0.05
-N  = 2
-T  = 0.05
-step = T/dt + 1
+N  = 10
+step = 11 
 
 f = open("Particle_position.txt","r")
 data = f.readlines()
@@ -23,18 +22,21 @@ z = []
 
 for a in range(step):
     for i in range(a*N,(a+1)*N,1 ):
-         sdata = data[i].split(" ")
-         x.extend(sdata[0])
-         y.extend(sdata[1])
-         z.extend(sdata[2])
+         sdata = data[i].split()
+         print(sdata)
+         l = float(sdata[0])
+         j = float(sdata[1])
+         k = float(sdata[2])
+         x.append(l)
+         y.append(j)
+         z.append(k)
     ax = plt.subplot(projection='3d')  
     ax.scatter(x, y, z, c='r')  
     ax.set_zlabel('Z')  
     ax.set_ylabel('Y')
     ax.set_xlabel('X')
     plt.draw()
-    plt.pause(10)
-    plt.savefig('time = ',step*dt)
+    plt.savefig('time = '+ str(a*dt) +'.png')
     plt.close()
     x = []
     y = []
